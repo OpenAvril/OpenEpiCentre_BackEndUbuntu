4064eb4c4853088bf1c4ccc48f4040df12f76fd5
OpenAvril <Jasper.Assembly@icloud.com>
2026-05-03 15:25:48 +1000

    BRAVO_TEST_DEVELOPMENT_2026_05_03_1525. implemented Linux based development.

---
+++ include/CLIB_WriteQueForThreadsAt_SERVERINPUTRECIEVE/WriteEnableForThreadsAt_SERVERINPUTRECEIVE_Framework.h
@@ -0,0 +1,24 @@
+#pragma once
+namespace OpenAvrilCLIBWriteEnableForThreadsAtSERVERINPUTRECEIVE
+{
+    class WriteEnableForThreadsAt_SERVERINPUTRECEIVE_Framework
+    {
+    public:
+        WriteEnableForThreadsAt_SERVERINPUTRECEIVE_Framework();
+        virtual ~WriteEnableForThreadsAt_SERVERINPUTRECEIVE_Framework();
+        static void boot0_CLASS_DECLAIRE_WriteEnableForThreadsAt_SERVERINPUTRECEIVE_Framework();
+        static void boot1_CLASS_DEFINE_WriteEnableForThreadsAt_SERVERINPUTRECEIVE_Framework();
+        static void boot3_CLASS_INITIALISE_WriteEnableForThreadsAt_SERVERINPUTRECEIVE_Framework();
+        static void boot0_REG_DECLAIRE_WriteEnableForThreadsAt_SERVERINPUTRECEIVE_Framework();
+        void boot1_REG_DEFINE_WriteEnableForThreadsAt_SERVERINPUTRECEIVE_Framework(class WriteEnableForThreadsAt_SERVERINPUTRECEIVE_Framework* obj);
+        void boot2_REG_SUBSTANTIATE_WriteEnableForThreadsAt_SERVERINPUTRECEIVE_Framework(class WriteEnableForThreadsAt_SERVERINPUTRECEIVE_Framework* obj);
+        void boot3_REG_INITIALISE_WriteEnableForThreadsAt_SERVERINPUTRECEIVE_Framework(class WriteEnableForThreadsAt_SERVERINPUTRECEIVE_Framework* obj);
+        void boot4_PGM_INSTANTIATION_WriteEnableForThreadsAt_SERVERINPUTRECEIVE_Framework(class WriteEnableForThreadsAt_SERVERINPUTRECEIVE_Framework* obj);
+        class WriteEnableForThreadsAt_SERVERINPUTRECEIVE* dyn_CLASS_get_ptr_WriteEnable();
+    private:
+        static class WriteEnableForThreadsAt_SERVERINPUTRECEIVE* _CLASS_get_ptr_WriteEnable;
+        static void stat_CLASS_boot1_DEFINE_WriteEnableForThreadsAt_SERVERINPUTRECEIVE();
+        static void stat_CLASS_boot3_INITIALISE_WriteEnableForThreadsAt_SERVERINPUTRECEIVE();
+        static class WriteEnableForThreadsAt_SERVERINPUTRECEIVE* stat_CLASS_get_ptr_WriteEnable();
+    };
+}
