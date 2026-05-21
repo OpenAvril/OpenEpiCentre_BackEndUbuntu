f3fe50052b2d4113e83557ada281e951a8e2dc15
OpenAvril <Jasper.Assembly@icloud.com>
2026-05-21 14:22:53 +1000

    DEVELOPMENT_2026_05_21_1422, <COMPILE_SUCCESS>.

---
+++ build/CMakeFiles/CLIB_OpenEpiCentre.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/CLIB_OpenEpiCentre.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/CLIB_OpenEpiCentre.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/CLIB_OpenEpiCentre.cpp.o.d"
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/engine/Algorithms.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Algorithms.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Algorithms.cpp.o.d"
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/engine/Data.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Data.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Data.cpp.o.d"
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/engine/Execute.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Execute.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Execute.cpp.o.d"
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/engine/Execute_Control.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Execute_Control.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Execute_Control.cpp.o.d"
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/engine/Framework_Server.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Framework_Server.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Framework_Server.cpp.o.d"
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/engine/Global.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Global.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Global.cpp.o.d"
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/engine/Server.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Server.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Server.cpp.o.d"
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/structs/Concurrent.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/Concurrent.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/Concurrent.cpp.o.d"
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/structs/Input.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/Input.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/Input.cpp.o.d"
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/structs/Output.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/Output.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/Output.cpp.o.d"
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/structs/User_Algorithm.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/User_Algorithm.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/User_Algorithm.cpp.o.d"
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/structs/User_Input.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/User_Input.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/User_Input.cpp.o.d"
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/structs/User_Output.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/User_Output.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/User_Output.cpp.o.d"
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/structs/praise_sets/Algorithm_praise0.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/praise_sets/Algorithm_praise0.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/praise_sets/Algorithm_praise0.cpp.o.d"
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/structs/praise_sets/Input_praise0.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/praise_sets/Input_praise0.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/praise_sets/Input_praise0.cpp.o.d"
+  "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/src/structs/praise_sets/Output_praise0.cpp" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/praise_sets/Output_praise0.cpp.o" "gcc" "CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/praise_sets/Output_praise0.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
