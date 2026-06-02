bfe3c18ecdac59c7e3f10812cc8d2ff593a9926d
myBCompSc <brentonjames.uni@icloud.com>
2026-06-02 11:51:00 +1000

    DEVELOPMENT_2026_06_02_1150, <COMPILE_SUCCESS>.
    implemented:
    - CMakeLists.txt.
    - imported updated libraries.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/brentonjames/CLionProjects/CLIB_OpenEpiCentre
+# Install script for directory: /home/brenton-james/CLionProjects/CLIB_OpenEpiCentre
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,7 +45,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/cmake-build-debug/install_local_manifest.txt"
+  file(WRITE "/home/brenton-james/CLionProjects/CLIB_OpenEpiCentre/cmake-build-debug/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -61,6 +61,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/brentonjames/CLionProjects/CLIB_OpenEpiCentre/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "/home/brenton-james/CLionProjects/CLIB_OpenEpiCentre/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
