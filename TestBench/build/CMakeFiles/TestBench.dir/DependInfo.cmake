7ca7db4c3d52085894ab34f57a969d5ce0dc5e48
myBCompSc <brentonjames.uni@icloud.com>
2026-06-04 14:50:49 +1000

    DEVELOPMENT_202_06_01_1205. <COMPILE_ERROR>.
    [ 66%] Linking CXX executable TestBench
    undefined reference to `CLIBOpenEpiCentre::Framework_Server::dyn_CLASS_get_ptr_Global()'
    
    implemented:
    - WriteQue CMakeLists.txt.
    - LaunchQue CMakeLists.txt.
    - OpenEpiCentre CMakeLists.txt.
    - TestBench CMkaeLists.txt.
       - fixed CLIB name.
    - fixed CMake import #include for libraries.
    - added missing data_control.cpp in openepicentre.
       - added member functions.
    
    todo:
    - build TestBench.
    - run SIMULATION.

---
+++ TestBench/build/CMakeFiles/TestBench.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/IO.cpp" "CMakeFiles/TestBench.dir/IO.cpp.o" "gcc" "CMakeFiles/TestBench.dir/IO.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/Main.cpp" "CMakeFiles/TestBench.dir/Main.cpp.o" "gcc" "CMakeFiles/TestBench.dir/Main.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/src/engine/Algorithms.cpp" "CMakeFiles/TestBench.dir/src/engine/Algorithms.cpp.o" "gcc" "CMakeFiles/TestBench.dir/src/engine/Algorithms.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/src/engine/App.cpp" "CMakeFiles/TestBench.dir/src/engine/App.cpp.o" "gcc" "CMakeFiles/TestBench.dir/src/engine/App.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/src/engine/Data.cpp" "CMakeFiles/TestBench.dir/src/engine/Data.cpp.o" "gcc" "CMakeFiles/TestBench.dir/src/engine/Data.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/src/engine/Execute.cpp" "CMakeFiles/TestBench.dir/src/engine/Execute.cpp.o" "gcc" "CMakeFiles/TestBench.dir/src/engine/Execute.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/src/engine/Execute_Control.cpp" "CMakeFiles/TestBench.dir/src/engine/Execute_Control.cpp.o" "gcc" "CMakeFiles/TestBench.dir/src/engine/Execute_Control.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/src/engine/Framework_App.cpp" "CMakeFiles/TestBench.dir/src/engine/Framework_App.cpp.o" "gcc" "CMakeFiles/TestBench.dir/src/engine/Framework_App.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/src/engine/Global.cpp" "CMakeFiles/TestBench.dir/src/engine/Global.cpp.o" "gcc" "CMakeFiles/TestBench.dir/src/engine/Global.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ TestBench/build/libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/CLIB_OpenEpiCentre.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/CLIB_OpenEpiCentre.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/CLIB_OpenEpiCentre.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/engine/Algorithms.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Algorithms.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Algorithms.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/engine/Data.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Data.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Data.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/engine/Data_Control.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Data_Control.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Data_Control.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/engine/Execute.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Execute.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Execute.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/engine/Execute_Control.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Execute_Control.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Execute_Control.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/engine/Framework_Server.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Framework_Server.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Framework_Server.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/engine/Global.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Global.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Global.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/engine/Server.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Server.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/engine/Server.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/structs/Concurrent.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/Concurrent.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/Concurrent.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/structs/Input.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/Input.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/Input.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/structs/Output.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/Output.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/Output.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/structs/User_Algorithm.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/User_Algorithm.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/User_Algorithm.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/structs/User_Input.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/User_Input.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/User_Input.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/structs/User_Output.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/User_Output.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/User_Output.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/structs/praise_sets/Algorithm_praise0.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/praise_sets/Algorithm_praise0.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/praise_sets/Algorithm_praise0.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/structs/praise_sets/Input_praise0.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/praise_sets/Input_praise0.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/praise_sets/Input_praise0.cpp.o.d"
+  "/home/brenton-james/CLionProjects/OpenEpiCentre_BackEndUbuntu/OpenEpiCentre_BackEndUbuntu/TestBench/libs/CLIB_OpenEpiCentre/src/structs/praise_sets/Output_praise0.cpp" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/praise_sets/Output_praise0.cpp.o" "gcc" "libs/CLIB_OpenEpiCentre/CMakeFiles/CLIB_OpenEpiCentre.dir/src/structs/praise_sets/Output_praise0.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
